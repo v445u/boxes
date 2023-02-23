d26578d79a498784291b2e2f277f9e56637d384b
Partha <vaasudhaiva013@gmail.com>
2023-02-24 00:42:27 +0530

    Add files via upload

---
+++ box/ftp_box/script.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+
+useradd henry -m -s /bin/bash -p l9yCE65T20D7oVRU
+cd /home/henry
+mkdir imp
+cd imp
+touch .ssssshhh
+echo "Freedom is not free... Earn... Sorry echo your pivelege!" | base64 | base64 > .ssssshhh
+echo "flag{__Y0u_g0t_h3nr1f13d__}" > user.flag
+chmod o+w /etc/passwd
+cd /root
+echo "flag{__Y0u_4r3_9r1v3l3g3d__}" > root.flag
